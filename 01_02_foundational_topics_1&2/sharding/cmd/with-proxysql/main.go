190113cea637c68fc1778daf44d8751dda9b198f
Rohan Gulati <rohangulati94@gmail.com>
2026-05-14 18:13:23 +0000

    added proxysql bsed routing (query comment hint)

--- 01_02_foundational_topics_1&2/sharding/cmd/with-proxysql/main.go
+++ 01_02_foundational_topics_1&2/sharding/cmd/with-proxysql/main.go
@@ -1,29 +1,67 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // var DBs = []*sql.DB{}
 
-func init() {
+func getDBConnection() *sql.DB {
+	username := "appuser"
+	password := "apppass"
+	host := "localhost"
+	port := "6033"
+	dbname := "app_db"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		username, password, host, port, dbname)
+
+	// Connect to the database
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Error in db connection: ", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error in db ping:", err)
+	}
+
+	// log.Println("Connected to database successfully!")
+
+	return db
+}
+
+func addShardHint(query string, userId int) string {
+	shard := 0
+	if userId == 1 {
+		shard = 1
+	} else {
+		shard = 0
+	}
+
+	return fmt.Sprintf("/* shard:%d */ %s", shard, query)
 }
 
 func addTask(userId int, task string) {
-	// res, err := DBs[getDBIndex(userId)].Exec(`INSERT INTO to_do_list (user_id, task) VALUES (?,?)`, userId, task)
-	// if err != nil {
-	// 	fmt.Println("Error adding task:", err)
-	// 	return
-	// }
-	// rowsAffected, err := res.RowsAffected()
-	// if err != nil {
-	// 	fmt.Println("Error getting rows affected:", err)
-	// 	return
-	// }
-	// fmt.Println("Task added successfully.", rowsAffected, "rows affected.")
+	db := getDBConnection()
+	query := addShardHint(`INSERT INTO to_do_list (user_id, task) VALUES (?,?)`, userId)
+	res, err := db.Exec(query, userId, task)
+	if err != nil {
+		fmt.Println("Error adding task:", err)
+		return
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("Error getting rows affected:", err)
+		return
+	}
+	fmt.Println("Task added successfully.", rowsAffected, "rows affected.")
 }
 
 func main() {
